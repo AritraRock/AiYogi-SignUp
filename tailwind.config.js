1c051a663f348184784e6cf77e4d63688eb1f4c2
Aritra <aritrabts@gmail.com>
2024-02-01 01:17:18 +0530

    'Commited'

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        lusitana: ['Lusitana', 'serif'],
+      },
+      colors: {
+        lightGray: "rgba(0, 0, 0, 0.58)",
+        darkGray: "rgba(0, 0, 0, 0.65)",
+      },
+    },
+  },
+  plugins: [],
+}
+
